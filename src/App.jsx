74fcb33876b1db4eac2f49970b2d00c8dca9294f
devDawson <dauxonphilip@gmail.com>
2025-07-16 20:14:43 +0300

    Initial commit - complete project structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import CTA from './components/CTA';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="font-sans bg-gray-50">
+      <Header />
+      <Hero />
+      <Services />
+      <CTA />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
